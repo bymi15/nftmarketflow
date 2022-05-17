48df20e35ce36aa9b27f86c32f91f387e40f93ea
Brian Min <bymi15@yahoo.com>
2022-05-17 23:57:28 +0900

    fix routes

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import Collection from 'pages/Collection';
 import CreateItem from 'pages/CreateItem';
 import Home from 'pages/Home';
 import Items from 'pages/Items';
-import { Navigate, Route, Routes as RoutesDOM } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes as RoutesDOM } from 'react-router-dom';
 
 const routes = [
   {
@@ -45,11 +45,13 @@ const routes = [
 
 export default function Routes() {
   return (
-    <RoutesDOM basename="/nftmarketflow">
-      {routes.map((route) => (
-        <Route exact path={route.route} element={<>{route.component}</>} key={route.key} />
-      ))}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </RoutesDOM>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <RoutesDOM>
+        {routes.map((route) => (
+          <Route exact path={route.route} element={<>{route.component}</>} key={route.key} />
+        ))}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </RoutesDOM>
+    </BrowserRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@mui/material/styles';
 import App from 'App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { GlobalContextProvider } from 'state/context';
 import theme from 'vui-theme/assets/theme';
 import { VisionUIControllerProvider } from 'vui-theme/context';
@@ -13,9 +12,7 @@ root.render(
   <ThemeProvider theme={theme}>
     <GlobalContextProvider>
       <VisionUIControllerProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </VisionUIControllerProvider>
     </GlobalContextProvider>
   </ThemeProvider>
